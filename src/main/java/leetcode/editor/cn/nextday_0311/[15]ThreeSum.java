159d303ff6621a8e25e049af704542b52040e60b
Jialin28 <Jialino102@gmail.com>
2020-03-10 15:31:45 +0800

    0310 新刷题

---
+++ src/main/java/leetcode/editor/cn/nextday_0311/[15]ThreeSum.java
@@ -0,0 +1,54 @@
+//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复
+//的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 示例： 
+//
+// 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+//
+//满足要求的三元组集合为：
+//[
+//  [-1, 0, 1],
+//  [-1, -1, 2]
+//]
+// 
+// Related Topics 数组 双指针
+
+package leetcode.editor.cn.nextday_0311;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length < 3) return result;
+        Arrays.sort(nums);
+        if(nums[0] > 0) return result;
+        for (int i = 0; i < nums.length; i++) {
+            if(i > 0  && nums[i] == nums[i-1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if(nums[left] + nums[right] + nums[i] == 0) {
+                    result.add(Arrays.asList(nums[left], nums[right], nums[i]));
+                    while(left<right && nums[left]==nums[left+1]) left++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
+                    left++;
+                    right--;
+                } else if(nums[left] + nums[right] + nums[i] > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
